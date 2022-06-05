e4f6b678fe51374703446131edc1bd1b22d7a94f
Alexis Wang <yingying8@student.unimelb.edu.au>
2022-06-05 13:25:32 +0800

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -81,11 +81,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
+    var contents = this.nextElementSibling;
+    if (contents.style.maxHeight){
+      contents.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      contents.style.maxHeight = contents.scrollHeight + "px";
     } 
   });
 }
